7d8083c5f4decb52a23ef59c50e536dcf9470d02
Nikita <nikitababb036@gmail.com>
2024-05-20 14:36:25 +0530

    created a webpage for analysis > co-authored-by: NIKITA320495 nikitababb036@gmail.com > co-authored-by: Leena2403 leenagoyal2403@gmail.com

---
+++ streamlit/functions.py
@@ -0,0 +1,316 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import pycountry
+import numpy as np
+from scipy.stats import norm
+import random
+from scipy.stats import norm
+import re
+
+data = pd.read_csv('df2020.csv')
+df2018 = pd.read_csv('df2018.csv')
+full_data2018 = pd.read_csv('../Data/survey_results_public_2018.csv')
+full_data2019=pd.read_csv('../Data/survey_results_public_2019.csv')
+full_df2020 = pd.read_csv('../Data/survey_results_public_2020.csv')
+df2019 = pd.read_csv('df2019.csv')
+df2020 = data[(data['SalaryUSD'] < 200000)]
+
+# features for job satisfaction
+results = pd.read_csv("results.csv")
+
+
+#######################################
+# VISUALISATION STARTS
+#######################################
+
+######-Nikita-########
+
+def plot_boxplot(data, x, y, title):
+    fig = go.Figure()
+    for group_name, group_data in data.groupby(x):
+        fig.add_trace(go.Box(y=group_data[y], name=group_name))
+    fig.update_layout(title=title, xaxis_title=x, yaxis_title=y)
+    st.plotly_chart(fig)
+
+#########################################################################    
+
+def plot_bar_plotly(df, column_name, top_n=10, height=450, width=700):
+    df_counts = df[column_name].value_counts().head(top_n).reset_index()
+    df_counts.columns = [column_name, 'Count']
+    
+    fig = px.bar(df_counts, x=column_name, y='Count', 
+                 labels={column_name: column_name, 'Count': 'Number of Developers'},
+                 color=column_name, color_discrete_sequence=px.colors.qualitative.Pastel)
+    
+    fig.update_layout(xaxis_title=column_name, yaxis_title='Number of Developers')
+    fig.update_layout(height=height, width=width)
+
+    return st.plotly_chart(fig)
+
+
+def plot_pie_plotly(df, column_name,top_n=10,  height=400, width=400 ):
+    participation_rate = df[column_name].value_counts().keys().tolist()[:top_n]
+    count = df[column_name].value_counts().tolist()[:top_n]
+
+    fig_pie = go.Figure(data=[go.Pie(labels=participation_rate, values=count)])
+    fig_pie.update_layout(title='Top {} Distribution'.format(column_name))
+    fig_pie.update_layout(height=height, width=width)
+
+    st.plotly_chart(fig_pie)    
+
+def plot_value_counts_plotly(df, column_name):
+    colors = ['lightseagreen', 'lightgreen', 'lightyellow', 'lightcoral', 'lightsalmon', 'lavender']
+
+    counts = df[column_name].value_counts()
+    fig = go.Figure(go.Bar(x=counts.index, y=counts.values, marker_color=random.choice(colors)))
+    fig.update_layout(title=f'Value Counts for {column_name}', xaxis_title='Response', yaxis_title='Count')
+    return fig
+
+
+def generate_normal_distribution_plots(df, column, top_n=10):
+    countries = df[column].value_counts().sort_values(ascending=False)[:top_n].index.tolist()
+
+    for country in countries:
+        temp_salaries = df.loc[df[column] == country, 'SalaryUSD']
+
+        #normal distribution curve
+        x_values = np.linspace(temp_salaries.min(), temp_salaries.max(), 100)
+        y_values = norm.pdf(x_values, temp_salaries.mean(), temp_salaries.std())
+
+        fig = go.Figure(data=go.Scatter(x=x_values, y=y_values))
+        
+        # mean line
+        fig.add_shape(type="line",
+                      x0=temp_salaries.mean(), y0=0,
+                      x1=temp_salaries.mean(), y1=norm.pdf(temp_salaries.mean(), temp_salaries.mean(), temp_salaries.std()),
+                      line=dict(color="red", width=2, dash="dash"))
+
+        fig.update_layout(title='Normal Distribution of Annual Salaries in {}'.format(country),
+                          xaxis_title="Annual Salary in USD",
+                          yaxis_title="Density")
+        fig.update_layout(height=400, width=370)
+
+        # st.plotly_chart(fig)
+        yield fig
+
+
+def plot_age_distribution(df, column_name):
+    df['Age_range'] = np.where((df[column_name] >= 15) & (df[column_name] <= 19), '15 - 19 years', 'Age_unknown')
+    df['Age_range'] = np.where((df[column_name] >= 20) & (df[column_name] <= 24), '20 - 24 years', df['Age_range'])
+    df['Age_range'] = np.where((df[column_name] >= 25) & (df[column_name] <= 29), '25 - 29 years', df['Age_range'])
+    df['Age_range'] = np.where((df[column_name] >= 30) & (df[column_name] <= 34), '30 - 34 years', df['Age_range'])
+    df['Age_range'] = np.where((df[column_name] >= 35) & (df[column_name] <= 39), '35 - 39 years', df['Age_range'])
+    df['Age_range'] = np.where((df[column_name] >= 40) & (df[column_name] <= 45), '40 - 45 years', df['Age_range'])
+    df['Age_range'] = np.where((df[column_name] >= 46), '46 and above years', df['Age_range'])
+
+    df_age = df.groupby(['Age_range']).size().reset_index(name='Count')
+    df_age.sort_values(by=['Count'], ascending=False, inplace=True)
+
+    # Plotly bar chart
+    fig = go.Figure(data=go.Bar(
+        x=df_age['Count'],
+        y=df_age['Age_range'],
+        orientation='h'
+    ))
+
+    # Update layout
+    fig.update_layout(
+        xaxis_title='Count',
+        yaxis_title='Age Range',
+        yaxis=dict(autorange="reversed")
+    )
+
+    st.plotly_chart(fig)
+
+def counts(df, column_name, year):
+    language_counts = df[column_name].str.split(';', expand=True).stack().value_counts().to_frame(name=year)
+    language_counts[column_name] = language_counts.index
+    language_counts.reset_index(drop=True, inplace=True)
+    language_counts = language_counts[[column_name, year]]
+    return language_counts
+
+def compare_column_and_plot(column):
+    languagedesire_2018 = counts(df2018, column, '2018')
+    languagedesire_2019 = counts(df2019, column, '2019')
+    languagedesire_2020 = counts(df2020, column, '2020')
+
+    # Merge language counts for both years
+    languagedesire_all = pd.merge(languagedesire_2018, languagedesire_2019, on=column, how='outer')
+    languagedesire_all = pd.merge(languagedesire_all, languagedesire_2020, on=column, how='outer')
+
+    
+    # Fill NaN values with 0 and convert counts to integers
+    languagedesire_all.fillna(0, inplace=True)
+    languagedesire_all['2018'] = languagedesire_all['2018'].astype(int)
+    languagedesire_all['2019'] = languagedesire_all['2019'].astype(int)
+    languagedesire_all['2020'] = languagedesire_all['2020'].astype(int)
+    
+    
+    languagedesire_all.set_index(column, inplace=True)
+
+    languagedesire19_20 = languagedesire_all.div(languagedesire_all.sum())
+
+    st.write(languagedesire19_20.head(5))
+    fig = go.Figure()
+
+    for column in languagedesire19_20.columns:
+        fig.add_trace(go.Bar(x=languagedesire19_20.index, y=languagedesire19_20[column], name=column))
+
+    fig.update_layout(
+        xaxis_title=column,
+        yaxis_title='Percentages',
+        font=dict(size=14),
+        barmode='group',
+        height=600,
+        width=800
+    )
+
+
+    st.plotly_chart(fig)
+
+def generate_choropleth(df, column_name):
+    grouped_df = df.groupby('Country').size().reset_index(name='Respondents')
+
+    # ISO country code from the country name
+    def get_country_code(name):
+        try:
+            return pycountry.countries.lookup(name).alpha_3
+        except LookupError:
+            return None
+
+    # Adding country code column
+    grouped_df['Country_code'] = grouped_df['Country'].apply(get_country_code)
+
+    #choropleth map
+    fig = px.choropleth(grouped_df,
+                        locations="Country_code",
+                        color=column_name,
+                        hover_name="Country",
+                        projection="natural earth",
+                        color_continuous_scale='Peach',
+                        range_color=[0, 10000],
+                        labels={column_name: 'Respondents'}
+                        )
+    fig.update_layout(height=600, width=900)
+    return st.plotly_chart(fig)
+
+def gender_vs_top5countries(df):
+    all_data = df.groupby(['Country', 'Gender']).size().reset_index(name='Count')
+    all_data['Total'] = all_data.groupby('Country')['Count'].transform('sum')
+    all_data['Percentage'] = all_data['Count'] / all_data['Total'] * 100
+
+
+    top_countries = all_data.groupby('Country')['Total'].max().nlargest(5).index
+    top_data = all_data[all_data['Country'].isin(top_countries)]
+
+    # men and women data
+    men_data = top_data[top_data['Gender'] == 'Man']
+    women_data = top_data[top_data['Gender'] == 'Woman']
+
+    fig = go.Figure()
+
+    #bars for 'Men'
+    fig.add_trace(go.Bar(x=men_data['Country'], y=men_data['Percentage'], name='Men', marker_color='darkblue'))
+
+    #bars for 'Women'
+    fig.add_trace(go.Bar(x=women_data['Country'], y=women_data['Percentage'], name='Women', marker_color='#5E96E9'))
+
+    fig.update_layout(
+        title='Gender vs Top 5 Countries in 2019',
+        xaxis_title='Top 5 Countries',
+        yaxis_title='Percentage',
+        barmode='group'
+    )
+
+    return fig
+def heighest_paying_2019():
+    ds = df2019[df2019['DevType'].str.contains('Data scientist') == True ]
+    ds_mean_salary = ds.groupby('Country')['SalaryUSD'].mean().reset_index(name='Mean')
+    ds_mean_salary.sort_values(by=['Mean'], ascending=False, inplace=True)
+    ds_mean_salary = ds_mean_salary[(ds_mean_salary['Mean'] <= 280000)]
+    Top_mean_salary = ds_mean_salary[:10]
+    
+    fig = px.bar(Top_mean_salary, x='Mean', y='Country', orientation='h',
+                 labels={'Mean': 'Average Salary in US$', 'Country': 'Country'},
+                 title='The Top 10 highest paying data scientist countries in 2019')
+    
+    fig.update_layout(yaxis={'categoryorder':'total ascending'}, 
+                      title={'x':0.5, 'xanchor': 'center', 'yanchor': 'top'})
+    st.plotly_chart(fig)
+def heighest_paying(df):
+    ds = df[df['DevType'].str.contains('Data scientist') == True ]
+    ds_mean_salary = ds.groupby('Country')['SalaryUSD'].mean().reset_index(name='Mean')
+    ds_mean_salary.sort_values(by=['Mean'], ascending=False, inplace=True)
+    ds_mean_salary = ds_mean_salary[(ds_mean_salary['Mean'] <= 280000)]
+    Top_mean_salary = ds_mean_salary[:10]
+    
+    fig = px.bar(Top_mean_salary, x='Mean', y='Country', orientation='h',
+                 labels={'Mean': 'Average Salary in US$', 'Country': 'Country'},
+                 title='The Top 10 highest paying data scientist countries ')
+    
+    fig.update_layout(yaxis={'categoryorder':'total ascending'}, 
+                      title={'x':0.5, 'xanchor': 'center', 'yanchor': 'top'})
+    st.plotly_chart(fig)
+def plot_value_counts_plotly(column_name, df, column):
+    values = df[column_name].value_counts()
+    fig = go.Figure(data=[go.Bar(x=values.index, y=values.values, marker_color=random.choice(['lightseagreen', 'lightgreen', 'lightyellow', 'lightcoral', 'lightsalmon', 'lavender']))])
+    fig.update_layout(title=f'Value Counts for {column_name}', xaxis_title='Response', yaxis_title='Count')
+    column.plotly_chart(fig)
+
+def ai_graphs():
+    st.title('AI Survey Responses')
+    df = full_data2018[['AIDangerous', 'AIInteresting', 'AIResponsible', 'AIFuture']]
+
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+    short_mapping = {
+        'Algorithms making important decisions': 'Algorithms',
+        'Artificial intelligence surpassing human intelligence ("the singularity")': 'AI Singularity',
+        'Evolving definitions of "fairness" in algorithmic versus human decisions': 'Fairness Evolution',
+        "Increasing automation of jobs": 'Automation',
+        "The developers or the people creating the AI": 'Developers',
+        "A governmental or other regulatory body": 'Government/Regulatory',
+        "Prominent industry leaders": 'Industry Leaders',
+        "Nobody": 'No Responsibility',
+        "I'm excited about the possibilities more than worried about the dangers.": 'Excited about AI Future',
+        "I'm worried about the dangers more than I'm excited about the possibilities.": 'Worried about AI Future',
+        "I don't care about it, or I haven't thought about it.": 'Indifferent about AI Future'
+    }
+
+    df.replace(short_mapping, inplace=True)
+
+    col1, col2 = st.columns(2)
+
+    plot_value_counts_plotly('AIDangerous', df, col1)
+    plot_value_counts_plotly('AIInteresting', df, col1)
+    plot_value_counts_plotly('AIResponsible', df, col2)
+    plot_value_counts_plotly('AIFuture', df, col2)   
+
+
+def result_plot(data):
+    new_index = data.Rates.sort_values(ascending=False).index
+    sorted_results = data.reindex(new_index)
+
+    filtered_results = sorted_results[np.abs(sorted_results.Rates) > 0.1]
+
+    #Plotly figure
+    fig = px.bar(
+        filtered_results,
+        x='Rates',
+        y='Columns',
+        orientation='h',
+        labels={'Rates': 'Negative and Positive Features', 'Columns': 'Features'},
+    )
+
+    fig.update_layout(
+        xaxis_title='Negative and Positive Features',
+        yaxis_title='Features',
+        title_font_size=25,
+        xaxis_title_font_size=25,
+        yaxis_title_font_size=25,
+        height=800,
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
---
+++ streamlit/main_analysis.py
@@ -0,0 +1,273 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import functions as ff
+
+data = pd.read_csv('df2020.csv')
+df2018 = pd.read_csv('df2018.csv')
+full_data2018 = pd.read_csv('../Data/survey_results_public_2018.csv')
+full_data2019=pd.read_csv('../Data/survey_results_public_2019.csv')
+full_df2020 = pd.read_csv('../Data/survey_results_public_2020.csv')
+df2019 = pd.read_csv('df2019.csv')
+df2020 = data[(data['SalaryUSD'] < 200000)]
+
+# features for job satisfaction
+results = pd.read_csv("results.csv")
+
+# for hightest paying ds
+full_data2018.rename(columns={'ConvertedSalary': 'SalaryUSD'}, inplace=True)
+
+def main_analysis(df):
+    st.title("StackOverflow Survey Analysis")
+
+    with st.expander("Data Preview"):
+        st.dataframe(df)
+
+    col_start1, col_start2, col_start3 = st.columns((1,1,1))
+    with col_start1:
+        ff.plot_pie_plotly(df, 'Gender', 100, 450, 500)
+    with col_start2:
+        ff.plot_pie_plotly(df, 'Employment', 100, 450, 520)
+    with col_start3:
+        ff.plot_pie_plotly(df, 'UndergradMajor', 100, 450, 500)    
+
+    plots = ff.generate_normal_distribution_plots(df, 'Country',top_n=3)
+
+    col_start1.plotly_chart(next(plots))
+    col_start2.plotly_chart(next(plots))
+    col_start3.plotly_chart(next(plots))
+
+    annual_salary_top_text = """
+    <div class='analysis-container-extra'>
+        <div class='analysis-title'>Analysis: Distribution of Annual Salary for Top Countries</div>
+        <div class='analysis-text'>
+            Overall, the country which has the highest mean annual salary is the United States of America(240,000) Dollars. The second highest country which provides the highest mean salary is Australia(164,926) Dollars. Though India has a higher number of respondents, it has the lowest mean salary of $25,213.We can understand that the mean salary of a developed country is much higher than that of a developing country.
+        </div>
+    </div>
+"""
+    st.markdown(annual_salary_top_text, unsafe_allow_html=True)
+
+    visual, analysis = st.columns((3,1))
+    
+    with visual:
+        st.title("Geographical Plot For Respondents")
+        ff.generate_choropleth(df, 'Respondents')
+
+    with analysis:
+        geographical_text = """
+    <div class='analysis-container'>
+        <div class='analysis-title'>Analysis: Geographical plot to show number of respondents in each country</div>
+        <div class='analysis-text'>
+            The geographical plot shows the number of respondents by country, with the United States having the highest participation. Other countries with significant participation include India, Brazil, and several European nations. The intensity of color represents the number of respondents, with darker shades indicating higher numbers.
+        </div>
+    </div>
+"""
+        st.markdown(geographical_text, unsafe_allow_html=True)
+
+    with visual:
+        st.title("Income VS Gender")
+        figBoxPlot = px.box(df, x='Gender', y='SalaryUSD', title='Income vs Gender')
+        st.plotly_chart(figBoxPlot, use_container_width=True)
+
+    with analysis:
+        income_gender_text = """
+        <div class='analysis-container'>
+            <div class='analysis-title'>Analysis: Income vs Gender</div>
+            <div class='analysis-text'>
+                There is a little bit of difference between Gender and income they received respectively. 
+                Men tend to receive more salary than women from the above analysis.
+            </div>
+        </div>
+        """
+        st.markdown(income_gender_text, unsafe_allow_html=True)
+
+
+    with visual:
+        st.title("Ethnicity VS Participation")
+        ff.plot_bar_plotly(df, 'Ethnicity')
+
+    with analysis:
+        Ethnicity_text = """
+        <div class='analysis-container'>
+            <div class='analysis-title'>Analysis: Ethnicity vs Participation</div>
+            <div class='analysis-text'>
+                From the Survey Analysis, more participation has been happened from White or of European Ethnicity.
+                The least has been recorded as only 0.16% from Indigenous. The second top survey contributors are from South Asians which is 11.93% of the respondents..
+            </div>
+        </div>
+    """
+        st.markdown(Ethnicity_text, unsafe_allow_html=True)
+
+    if (df is df2019 or df is df2020):
+        with visual:
+            st.title("Distribution of Age of Respondents")
+            ff.plot_age_distribution(df, 'Age')
+
+        with analysis:
+            age_text = """
+            <div class='analysis-container-extra'>
+                <div class='analysis-title'>Analysis: Distribution of respondents based on age</div>
+                <div class='analysis-text'>
+                    Late twenties respondents are clearly dominating the survey responses. It could be the age-range of a typical user on StackOverflow website.
+                    The graph is plotted in a descending order for better visuality, and understanding.
+            </div>
+        """
+            st.markdown(age_text, unsafe_allow_html=True)
+
+    with visual:
+        st.title("Men vs Women Participation in Countries with Highest Respondents")
+        st.plotly_chart(ff.gender_vs_top5countries(df))
+
+    with analysis:
+        gender_top_country_text = """
+    <div style='margin-top: 400px !important' class='analysis-container'>
+        <div class='analysis-title'>Analysis: Men vs Women Participation</div>
+        <div class='analysis-text'>
+            Women participation is extremely low in the STEM field, compared to men. They are even less than 20% of total male population that is dominating the tech industry in almost all the countries.
+        </div>
+    </div>
+"""     
+        st.markdown(gender_top_country_text, unsafe_allow_html=True)        
+
+    with visual:    
+        st.title("Comparing Language Preferences Across Years")
+        ff.compare_column_and_plot("LanguageDesireNextYear")
+
+    with analysis:    
+        LanguageDesireNextYear_text = """
+        <div class='analysis-container'>
+            <div class='analysis-title'>Analysis: Programming language desired to work</div>
+            <div class='analysis-text'>
+                In 2019, respondents said that they wanted to work in javascript is around more than 10 % and the fewer respond have a desire to work on VBA next year. People started to work in Haskell, Julia, and pearl in 2019 though the amount was less around 5% of people have the desire to work in those languages in 2021. Here, phyton is the 2nd one in which people have the desire to work in both 2019 and 2020.
+                \n However, if we look at the big picture, Python has been constantly gaining significant popularity within the developers community for three consequent years, whereas JavaScript is either constant or decling in popularity.
+        </div>
+    """
+        st.markdown(LanguageDesireNextYear_text, unsafe_allow_html=True)
+    
+
+    st.title("Education Level VS Salary")
+    plots = ff.generate_normal_distribution_plots(df, 'EdLevel',top_n=6)
+
+    col_1, col_2, col_3 = st.columns((1, 1, 1))
+
+    col_1.plotly_chart(next(plots))
+    col_2.plotly_chart(next(plots))
+    col_3.plotly_chart(next(plots))
+    col_1.plotly_chart(next(plots))
+    col_2.plotly_chart(next(plots))
+    col_3.plotly_chart(next(plots))
+
+    education_salary_text = """
+            <div class='analysis-container-extra'>
+                <div class='analysis-title'>Analysis: Programming language desired to work</div>
+                <div class='analysis-text'>
+                    As we can see, the respondents who have done Doctorate have the highest mean salary among all other education levels. Secondly, the respondents who have done Bachelors degree have more salary than that of Masters degree holders. This may be due to years of professional coding experience and due to the higher number of respondents in that category than that of Masters degree
+                    \nThe most interesting is that the respondents who do not have any degree have a mean salary of $90k. This shows the improvement in online learning and advancement of technology that is shifting the company from relying on University degrees.
+            </div>
+        """
+    st.markdown(education_salary_text, unsafe_allow_html=True)
+
+    with visual:
+        st.title("Distribution of surveyors based on their developer role")
+        ff.compare_column_and_plot("DevType")
+
+    with analysis:   
+        devtype_text = """
+        <div style='margin-top: 400px !important' class='analysis-container'>
+            <div class='analysis-title'>Analysis: Distribution of surveyors based on their developer role</div>
+            <div class='analysis-text'>
+                Based on respondents responses the survey concluded that they wanted to work in JavaScript is around more than 10%, and fewer respondents have a desire to work on VBA next year. People started to work in Haskell, Julia, and Pearl in 2019, though the amount was less; around 5% of people have the desire to work in those languages in 2021. Here, Python is the 2nd one in which people have the desire to work in both 2019 and 2020.
+                \nHowever, if we look at the big picture, Python has been constantly gaining significant popularity within the developer community for three consequent years, whereas JavaScript is either constant or declining in popularity.
+            </div>
+        </div>
+    """
+        st.markdown(devtype_text, unsafe_allow_html=True)
+
+
+    ds = None
+
+    if df is df2019:
+        ds = df2019[df2019.reset_index()['DevType'].str.contains('Data scientist or machine learning specialist') == True]
+        ds = ds.reset_index(drop=True)
+    elif df is df2020:
+        ds = df2020[df2020['DevType'].str.contains('Data scientist or machine learning specialist') == True ]
+        ds = ds.reset_index(drop=True)   
+    else:
+        ds = full_data2018[full_data2018.reset_index()['DevType'].str.contains('Data scientist or machine learning specialist') == True]
+        ds = ds.reset_index(drop=True) 
+
+    if ds is not None: 
+        with visual:
+            st.title("Country Wise Data Scientists Participation")
+            ff.plot_bar_plotly(ds, "Country")
+
+    with analysis:     
+        data_scientist_participation_text = """
+    <div style='margin-top: 400px !important' class='analysis-container'>
+        <div class='analysis-title'>Analysis: Data Scientist Market</div>
+        <div class='analysis-text'>
+            There are many data scientists who responded to the Stackoverflow survey. Most data scientists are from the US around 1,500-1700 people and it is 3 times higher than data scientists from India. Followed by Germany and the UK with 427 and 339 people respectively. The rest are Canada, France, Netherlands, Brazil, Russia, and Australia which have less than 200 data scientists.
+        </div>
+    </div>
+"""
+        st.markdown(data_scientist_participation_text, unsafe_allow_html=True)
+
+    # finding top paying countries for data scientists
+    '''if df is df2019:
+        with visual:
+            st.title("Highest Paying Countries for Data Scientists")
+            ff.heighest_paying_2019()
+    elif df is df2020:
+        with visual:
+            st.title("Highest Paying Countries for Data Scientists")
+            ff.heighest_paying(df)
+    else:
+        with visual:
+            st.title("Highest Paying Countries for Data Scientists")
+            ff.heighest_paying(full_data2018)
+
+    with analysis:
+        highest_paying_ds_text = """
+    <div style='margin-top: 400px !important' class='analysis-container'>
+        <div class='analysis-title'>Analysis: Data Scientist Market</div>
+        <div class='analysis-text'>
+            The top  countries which have a highest mean annual salary of a data scientist are South Korea (253,315) in 2018,Ireland (275,851) in 2019, and the USA(118,863) in 2020. Apart from that, the mean salary of the rest of the countries is less than (200,000) per year. Japan provides the highest mean annual salary among Asian countries (118,969)
+            Figures in Dollars $
+        </div>
+    </div>
+"""
+        st.markdown(highest_paying_ds_text, unsafe_allow_html=True)'''
+
+
+    if df is df2019:
+        with visual:
+            ff.plot_bar_plotly(df, 'JobSatisfaction')   
+
+        with analysis:
+              jobsatis_text = """
+    <div style='margin-top: 400px !important' class='analysis-container'>
+        <div class='analysis-title'>Analysis: Data Scientist Market</div>
+        <div class='analysis-text'>
+            In 2019, the top three countries which have a highest mean annual salary of a data scientist are Ireland (275,851), Luxembourg (272,769), and the USA (265,211). Apart from that, the mean salary of the rest of the countries is less than (200,000) per year. Japan provides the highest mean annual salary among Asian countries (118,969)
+            Figures in Dollars $
+        </div>
+    </div>
+"""     
+        st.markdown(jobsatis_text, unsafe_allow_html=True)
+
+    with visual:
+        st.title("Features for Job Satisfaction")
+        ff.result_plot(results)
+    with analysis:
+        feature_jobsatis_text = """
+    <div style='margin-top: 400px !important' class='analysis-container'>
+        <div class='analysis-title'>Analysis: Features for Job Satisfaction</div>
+        <div class='analysis-text'>
+            The top 2 features negatively affecting Job Satisfaction are age, country. So, in the elderly ages, job satisfaction may decrease because of the personal expectation increases. In the same way, as the professional coding years are increasing, satisfaction may decrease.
+            Among the countries; most dissatisfied countries are Angolia, Rwanda, Krygyzstan, Sudan.
+            UndergradMajor and other Science, are mostly satisfied.
+            Most satisfied countries Malta, Ghana, Cyprus.
+        </div>
+    </div>
+"""     
+        st.markdown(feature_jobsatis_text, unsafe_allow_html=True)
