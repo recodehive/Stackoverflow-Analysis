2ea89c20b30ced11d2d6cd92b38f895079833974
revanth1718 <revanthpasupuleti18@gmail.com>
2024-08-09 11:02:25 +0530

    bug fixed

--- streamlit/main_analysis.py
+++ streamlit/main_analysis.py
@@ -9,8 +9,8 @@ full_data2018 = pd.read_csv('https://raw.githubusercontent.com/Recode-Hive/Stack
 full_data2019=pd.read_csv('https://raw.githubusercontent.com/Recode-Hive/Stackoverflow-Analysis/main/streamlit/survey_results_sample_2019.csv')
 full_df2020 = pd.read_csv('https://raw.githubusercontent.com/Recode-Hive/Stackoverflow-Analysis/main/streamlit/survey_results_sample_2020.csv')
 df2019 = pd.read_csv('https://raw.githubusercontent.com/Recode-Hive/Stackoverflow-Analysis/main/streamlit/df2019.csv')
-df2021 = pd.read_csv('df2021.csv')
-df2022 = pd.read_csv('df2022.csv')
+df2021 = pd.read_csv('https://raw.githubusercontent.com/Recode-Hive/Stackoverflow-Analysis/main/streamlit/df2021.csv')
+df2022 = pd.read_csv('https://raw.githubusercontent.com/Recode-Hive/Stackoverflow-Analysis/main/streamlit/df2022.csv')
 df2020 = data[(data['SalaryUSD'] < 200000)]
 
 # Changes made in df2021 and df2022:
